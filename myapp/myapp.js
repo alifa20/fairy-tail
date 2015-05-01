f9b6f3d41efa4aceb03cf84cf0df955a028fe93c
alifa20 <ali.fathieh@gmail.com>
2015-05-02 01:43:23 +1000

    in done #1

--- myapp/myapp.js
+++ myapp/myapp.js
@@ -1,23 +1,40 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+MainLine = new Mongo.Collection("main_story");
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+  }
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+  if (Meteor.isClient) {
+    // This code only runs on the client
+    Template.body.helpers({
+      main_stories: function () {
+        return MainLine.find({});
+      }
+    });
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Template.body.events({
+      "click .cmd_btn":function(event){
+        var d = new Date();
+        MainLine.insert({
+          text: "button clicked " + d.getHours() +":"+d.getMinutes()+":"+d.getSeconds(),
+          createdAt: new Date()
+        });
+        return false;
+      }
+
+    });
+  }
+
+  if (Meteor.isServer) {
+    Meteor.startup(function () {
     // code to run on server at startup
   });
-}
+  }
